08cc81c4abd0320c14e7d035cab748580894bdca
SunilSA <sunil.sa@mindtree.com>
2019-07-10 12:34:57 +0530

    Ellipses Implementation

--- src/app/features/resources/center/center-header/center-header.component.ts
+++ src/app/features/resources/center/center-header/center-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CenterHeaderModel } from 'src/app/core/models/center-header.model';
+import { DataStorageService } from 'src/app/core/services/data-storage.service';
 
 @Component({
   selector: 'app-center-header',
@@ -7,17 +8,25 @@ import { CenterHeaderModel } from 'src/app/core/models/center-header.model';
   styleUrls: ['./center-header.component.scss']
 })
 export class CenterHeaderComponent implements OnInit {
+  actionButtonElipses = new Array();
 
   @Input() headerData: CenterHeaderModel;
 
+
   elipses = {
     type: 'elipses',
-    menuList: ['']
+    menuList: this.actionButtonElipses
   };
 
-  constructor() { }
+  constructor(private dataSerice: DataStorageService) { }
 
   ngOnInit() {
-    //   console.log('ScreenWidth', this.screenResize);
+    this.dataSerice.getCenterSpecificLabelsAndActions().subscribe(data => {
+      if (data && data.eng && data.eng.actionButtons) {
+        for (const list of data.eng.actionButtons) {
+          this.actionButtonElipses.push(list);
+        }
+      }
+    });
   }
 }
