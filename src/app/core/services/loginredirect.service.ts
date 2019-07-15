cf493ddbf4250ed2deb765169222509c43a79ec4
Ajay J <M1044588@mindtree.com>
2019-07-15 14:24:24 +0530

    enabling login

--- src/app/core/services/loginredirect.service.ts
+++ src/app/core/services/loginredirect.service.ts
@@ -8,10 +8,10 @@ export class LoginRedirectService {
 
   constructor(private cookie: CookieService) { }
 
-  // redirect(url: string) {
-  //   const stateParam = uuid();
-  //   this.cookie.set('state', stateParam, undefined, '/');
-  //  // console.log('returning false login redirect' + stateParam);
-  //   window.location.href = `${config.baseUrl}/authmanager/login/` + btoa(url);
-  // }
+  redirect(url: string) {
+    const stateParam = uuid();
+    this.cookie.set('state', stateParam, undefined, '/');
+   // console.log('returning false login redirect' + stateParam);
+    window.location.href = `${config.baseUrl}/authmanager/login/` + btoa(url);
+  }
 }
