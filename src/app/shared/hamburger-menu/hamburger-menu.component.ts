9282dff3ef1ad9ea58bd6b8cf4a8b6eaa426d8b0
Agnitra Banerjee <agneetra10@gmail.com>
2019-07-16 12:26:44 +0530

    Header data hardcode removed

--- src/app/shared/hamburger-menu/hamburger-menu.component.ts
+++ src/app/shared/hamburger-menu/hamburger-menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
-import { Location } from '@angular/common';
+import { HeaderService } from 'src/app/core/services/header.service';
 
 @Component({
   selector: 'app-hamburger-menu',
@@ -13,7 +13,7 @@ export class HamburgerComponent implements OnInit {
 
   dataList: any[];
 
-  constructor(private location: Location) { }
+  constructor(public headerService: HeaderService) { }
 
   ngOnInit() {
     console.log('hfdasfdsf', this.data);
