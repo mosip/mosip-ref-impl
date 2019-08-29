9294d66ed1e1f7c869b9026f20befdfcb2bbdc79
Agnitra Banerjee <agneetra10@gmail.com>
2019-08-29 16:28:42 +0530

    Packet status structure added

--- admin-ui/src/app/features/packet-status/packet-status.module.ts
+++ admin-ui/src/app/features/packet-status/packet-status.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PacketStatusComponent } from './packet-status/packet-status.component';
 import { PacketStatusRoutingModule } from './packet-status-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    PacketStatusRoutingModule
+    PacketStatusRoutingModule,
+    SharedModule,
+    FormsModule
   ],
   declarations: [PacketStatusComponent]
 })
