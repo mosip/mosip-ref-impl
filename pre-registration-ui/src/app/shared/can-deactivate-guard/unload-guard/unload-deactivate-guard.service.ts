69e581d8f4ae0aa3baad0ae3ce2ca70062ca940e
Shashank0707 <shashank.agrawal@mindtree.com>
2019-09-13 16:50:48 +0530

    MOS-28804 : Alert heaeder

--- pre-registration-ui/src/app/shared/can-deactivate-guard/unload-guard/unload-deactivate-guard.service.ts
+++ pre-registration-ui/src/app/shared/can-deactivate-guard/unload-guard/unload-deactivate-guard.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, HostListener } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
+import { MatDialog } from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
 })
 export abstract class UnloadDeactivateGuardService {
   abstract canDeactivate(): boolean;
-
+  constructor(public dialog: MatDialog) {}
   // @HostListener('window:beforeunload', ['$event'])
   // unloadNotification($event: any) {
   //   if (!this.canDeactivate()) {
