312698f3e3c3ed28d52f4f608cf7d1d956283ba3
Ajay1596 <44763643+Ajay1596@users.noreply.github.com>
2020-02-06 13:21:56 +0530

    MOSIP-37 and MOS-30605 adding verion in Ui application (#20)

--- pre-registration-ui/src/app/core/footer/footer.component.ts
+++ pre-registration-ui/src/app/core/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { version } from 'package.json';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  appVersion = version;
   constructor() { }
 
   ngOnInit() {
