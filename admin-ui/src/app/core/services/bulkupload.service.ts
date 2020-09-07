a427596d3ffb4ef1840f2c695a5c7c769112ee98
Aravindhan Alagesan <aravindh2090@gmail.com>
2020-09-07 22:09:19 +0530

    Removed unused imports from the files.

--- admin-ui/src/app/core/services/bulkupload.service.ts
+++ admin-ui/src/app/core/services/bulkupload.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { RequestModel } from '../models/request.model';
 import { Observable } from 'rxjs';
 import { AppConfigService } from 'src/app/app-config.service';
