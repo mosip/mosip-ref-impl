60b92290369ec4b51f8841af724cc81506ba1f5e
Aravindhan Alagesan <aravindh2090@gmail.com>
2020-11-03 16:01:03 +0530

    Build failed in dev env due to some case sensitive issue is fixed

--- admin-ui/src/app/features/keymanager/keymanager-routing.module.ts
+++ admin-ui/src/app/features/keymanager/keymanager-routing.module.ts
@@ -4,10 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'generatecsr', pathMatch: 'full' },
-  { path: 'generatecsr', loadChildren: () => import('../keymanager/generatecsr/generatecsr.module').then(m => m.GeneratecsrModule) },
-  { path: 'generatemasterkey', loadChildren: () => import('../keymanager/generatemasterkey/generatemasterkey.module').then(m => m.GeneratemasterkeyModule) },
-  { path: 'uploadcertificate', loadChildren: () => import('../keymanager/uploadcertificate/uploadcertificate.module').then(m => m.UploadcertificateModule) },
-  { path: 'uploadotherdomaincertificate', loadChildren: () => import('../keymanager/uploadotherdomaincertificate/uploadotherdomaincertificate.module').then(m => m.UploadotherdomaincertificateModule) },
+  { path: 'generatecsr', loadChildren: () => import('../keymanager/generateCSR/generatecsr.module').then(m => m.GeneratecsrModule) },
+  { path: 'generatemasterkey', loadChildren: () => import('../keymanager/generateMasterKey/generatemasterkey.module').then(m => m.GeneratemasterkeyModule) },
+  { path: 'uploadcertificate', loadChildren: () => import('../keymanager/uploadCertificate/uploadcertificate.module').then(m => m.UploadcertificateModule) },
+  { path: 'uploadotherdomaincertificate', loadChildren: () => import('../keymanager/uploadOtherDomainCertificate/uploadotherdomaincertificate.module').then(m => m.UploadotherdomaincertificateModule) },
 ];
 
 @NgModule({
