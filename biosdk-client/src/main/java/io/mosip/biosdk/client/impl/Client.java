e979cfebdf430d3d5904d5392500d1ab85fd9266
Ankit Vaishnav <ankitvaishnav.iitb1991@gmail.com>
2020-12-03 12:52:01 +0530

    BioSDK modules added

---
+++ biosdk-client/src/main/java/io/mosip/biosdk/client/impl/Client.java
@@ -0,0 +1,186 @@
+package io.mosip.biosdk.client.impl;
+
+import com.google.gson.Gson;
+import io.mosip.biosdk.client.dto.*;
+import io.mosip.biosdk.client.utils.Util;
+import io.mosip.kernel.biometrics.constant.BiometricType;
+import io.mosip.kernel.biometrics.entities.BiometricRecord;
+import io.mosip.kernel.biometrics.model.MatchDecision;
+import io.mosip.kernel.biometrics.model.QualityCheck;
+import io.mosip.kernel.biometrics.model.Response;
+import io.mosip.kernel.biometrics.model.SDKInfo;
+import io.mosip.kernel.biometrics.spi.IBioApi;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The Class BioApiImpl.
+ * 
+ * @author Sanjay Murali
+ * @author Manoj SP
+ * 
+ */
+public class Client implements IBioApi {
+
+	Logger LOGGER = LoggerFactory.getLogger(Client.class);
+
+	private static final String API_VERSION = "0.9";
+	private static final boolean rest = true;
+
+	/* "http://localhost:9099/biosdk-service" */
+	private static final String host = System.getenv("mosip_biosdk_service");
+
+	@Override
+	public SDKInfo init(Map<String, String> initParams) {
+		InitRequestDto initRequestDto = new InitRequestDto();
+		initRequestDto.setInitParams(initParams);
+
+		ResponseEntity<?> responseEntity = Util.restRequest(host+"/init", HttpMethod.POST, MediaType.APPLICATION_JSON, initRequestDto, null, String.class);
+		String responseBody = responseEntity.getBody().toString();
+		SDKInfo sdkInfo = null;
+		try {
+			JSONParser parser = new JSONParser();
+			JSONObject js = (JSONObject) parser.parse(responseBody);
+			Gson gson = new Gson();
+			sdkInfo = gson.fromJson(js.get("response").toString(), SDKInfo.class);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return sdkInfo;
+	}
+
+	@Override
+	public Response<QualityCheck> checkQuality(BiometricRecord sample, List<BiometricType> modalitiesToCheck, Map<String, String> flags) {
+		Response<QualityCheck> response = new Response<>();
+		response.setStatusCode(200);
+		CheckQualityRequestDto checkQualityRequestDto = new CheckQualityRequestDto();
+		checkQualityRequestDto.setSample(sample);
+		checkQualityRequestDto.setModalitiesToCheck(modalitiesToCheck);
+		checkQualityRequestDto.setFlags(flags);
+
+		ResponseEntity<?> responseEntity = Util.restRequest(host+"/check-quality", HttpMethod.POST, MediaType.APPLICATION_JSON, checkQualityRequestDto, null, String.class);
+		String responseBody = responseEntity.getBody().toString();
+		QualityCheck qualityCheck = null;
+		try {
+			JSONParser parser = new JSONParser();
+			JSONObject js = (JSONObject) parser.parse(responseBody);
+			Gson gson = new Gson();
+			qualityCheck = gson.fromJson(js.get("response").toString(), QualityCheck.class);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		response.setResponse(qualityCheck);
+		return response;
+	}
+
+	@Override
+	public Response<MatchDecision[]> match(BiometricRecord sample, BiometricRecord[] gallery,
+										   List<BiometricType> modalitiesToMatch, Map<String, String> flags) {
+		Response<MatchDecision[]> response = new Response<>();
+		MatchRequestDto matchRequestDto = new MatchRequestDto();
+		matchRequestDto.setSample(sample);
+		matchRequestDto.setGallery(gallery);
+		matchRequestDto.setModalitiesToMatch(modalitiesToMatch);
+		matchRequestDto.setFlags(flags);
+
+		ResponseEntity<?> responseEntity = Util.restRequest(host+"/match", HttpMethod.POST, MediaType.APPLICATION_JSON, matchRequestDto, null, String.class);
+		String responseBody = responseEntity.getBody().toString();
+		try {
+			JSONParser parser = new JSONParser();
+			JSONObject js = (JSONObject) parser.parse(responseBody);
+			Gson gson = new Gson();
+			JSONObject jsonResponse = (JSONObject) parser.parse(js.get("response").toString());
+			response.setStatusCode(((Long)jsonResponse.get("statusCode")).intValue());
+			response.setStatusMessage(
+				jsonResponse.get("statusMessage") != null ? jsonResponse.get("statusMessage").toString() : ""
+			);
+			response.setResponse(gson.fromJson(jsonResponse.get("response").toString(), MatchDecision[].class));
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return response;
+	}
+
+	@Override
+	public Response<BiometricRecord> extractTemplate(BiometricRecord sample, List<BiometricType> modalitiesToExtract, Map<String, String> flags) {
+		Response<BiometricRecord> response = new Response<>();
+		ExtractTemplateRequestDto extractTemplateRequestDto = new ExtractTemplateRequestDto();
+		extractTemplateRequestDto.setSample(sample);
+		extractTemplateRequestDto.setModalitiesToExtract(modalitiesToExtract);
+		extractTemplateRequestDto.setFlags(flags);
+		ResponseEntity<?> responseEntity = Util.restRequest(host+"/extract-template", HttpMethod.POST, MediaType.APPLICATION_JSON, extractTemplateRequestDto, null, String.class);
+		String responseBody = responseEntity.getBody().toString();
+		BiometricRecord resBiometricRecord = null;
+		try {
+			JSONParser parser = new JSONParser();
+			JSONObject js = (JSONObject) parser.parse(responseBody);
+			Gson gson = new Gson();
+			response = gson.fromJson(js.get("response").toString(), Response.class);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return response;
+	}
+
+	@Override
+	public Response<BiometricRecord> segment(BiometricRecord biometricRecord, List<BiometricType> modalitiesToSegment, Map<String, String> flags) {
+		Response<BiometricRecord> response = new Response<>();
+		SegmentRequestDto segmentRequestDto = new SegmentRequestDto();
+		segmentRequestDto.setSample(biometricRecord);
+		segmentRequestDto.setModalitiesToSegment(modalitiesToSegment);
+		segmentRequestDto.setFlags(flags);
+
+		ResponseEntity<?> responseEntity = Util.restRequest(host+"/segment", HttpMethod.POST, MediaType.APPLICATION_JSON, segmentRequestDto, null, String.class);
+		String responseBody = responseEntity.getBody().toString();
+		BiometricRecord resBiometricRecord = null;
+		try {
+			JSONParser parser = new JSONParser();
+			JSONObject js = (JSONObject) parser.parse(responseBody);
+			Gson gson = new Gson();
+			response = gson.fromJson(js.get("response").toString(), Response.class);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return response;
+	}
+
+	@Override
+	public BiometricRecord convertFormat(BiometricRecord sample, String sourceFormat, String targetFormat,
+			Map<String, String> sourceParams, Map<String, String> targetParams, List<BiometricType> modalitiesToConvert) {
+		ConvertFormatRequestDto convertFormatRequestDto = new ConvertFormatRequestDto();
+		convertFormatRequestDto.setSample(sample);
+		convertFormatRequestDto.setSourceFormat(sourceFormat);
+		convertFormatRequestDto.setTargetFormat(targetFormat);
+		convertFormatRequestDto.setSourceParams(sourceParams);
+		convertFormatRequestDto.setTargetParams(targetParams);
+		convertFormatRequestDto.setModalitiesToConvert(modalitiesToConvert);
+
+		ResponseEntity<?> responseEntity = Util.restRequest(host+"/convert-format", HttpMethod.POST, MediaType.APPLICATION_JSON, convertFormatRequestDto, null, String.class);
+		String responseBody = responseEntity.getBody().toString();
+		BiometricRecord resBiometricRecord = null;
+		try {
+			JSONParser parser = new JSONParser();
+			JSONObject js = (JSONObject) parser.parse(responseBody);
+			Gson gson = new Gson();
+			resBiometricRecord = gson.fromJson(js.get("response").toString(), BiometricRecord.class);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return resBiometricRecord;
+	}
+
+
+}
---
+++ biosdk-services/src/main/java/io/mosip/biosdk/services/controller/MainController.java
@@ -0,0 +1,145 @@
+package io.mosip.biosdk.services.controller;
+
+import com.google.gson.Gson;
+import io.mosip.biosdk.services.config.LoggerConfig;
+import io.mosip.biosdk.services.dto.*;
+import io.mosip.biosdk.services.utils.Utils;
+import io.mosip.kernel.biometrics.spi.IBioApi;
+import io.mosip.kernel.core.logger.spi.Logger;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/")
+@Api(tags = "Sdk")
+@CrossOrigin("*")
+public class MainController {
+
+    private Logger log = LoggerConfig.logConfig(MainController.class);
+
+    private String version = "1.0";
+
+    @Autowired
+    private Utils serviceUtil;
+
+    @Autowired
+    private IBioApi iBioApi;
+
+    @Autowired
+    private Gson gson;
+
+    @GetMapping(path = "/")
+    @ApiOperation(value = "Service status")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Service is running...") })
+    public ResponseEntity<String> status() {
+        Date d = new Date();
+        return ResponseEntity.status(HttpStatus.OK).body("Service is running... "+d.toString());
+    }
+
+    @PostMapping(path = "/init", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Initialization")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Initialization successful") })
+    public ResponseEntity<String> init(
+            @Validated @RequestBody(required = true) InitRequestDto request,
+            @ApiIgnore Errors errors) {
+        ResponseDto response = new ResponseDto();
+        response.setVersion(version);
+        response.setResponsetime(serviceUtil.getCurrentResponseTime());
+        response.setResponse(iBioApi.init(request.getInitParams()));
+        return ResponseEntity.status(HttpStatus.OK).body(gson.toJson(response));
+    }
+
+    @PostMapping(path = "/match", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Match")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Match successful") })
+    public ResponseEntity<String> match(
+            @Validated @RequestBody(required = true) MatchRequestDto request,
+            @ApiIgnore Errors errors) {
+        ResponseDto response = new ResponseDto();
+        response.setVersion(version);
+        response.setResponsetime(serviceUtil.getCurrentResponseTime());
+        response.setResponse(
+            iBioApi.match(request.getSample(), request.getGallery(), request.getModalitiesToMatch(), request.getFlags())
+        );
+        return ResponseEntity.status(HttpStatus.OK).body(gson.toJson(response));
+    }
+
+    @PostMapping(path = "/check-quality", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Check quality")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Check successful") })
+    public ResponseEntity<String> checkQuality(
+            @Validated @RequestBody(required = true) CheckQualityRequestDto request,
+            @ApiIgnore Errors errors) {
+        ResponseDto response = new ResponseDto();
+        response.setVersion(version);
+        response.setResponsetime(serviceUtil.getCurrentResponseTime());
+        response.setResponse(
+            iBioApi.checkQuality(request.getSample(), request.getModalitiesToCheck(), request.getFlags())
+        );
+        return ResponseEntity.status(HttpStatus.OK).body(gson.toJson(response));
+    }
+
+    @PostMapping(path = "/extract-template", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Extract template")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Extract successful") })
+    public ResponseEntity<String> extractTemplate(
+            @Validated @RequestBody(required = true) ExtractTemplateRequestDto request,
+            @ApiIgnore Errors errors) {
+        ResponseDto response = new ResponseDto();
+        response.setVersion(version);
+        response.setResponsetime(serviceUtil.getCurrentResponseTime());
+        response.setResponse(
+            iBioApi.extractTemplate(request.getSample(), request.getModalitiesToExtract(), request.getFlags())
+        );
+        return ResponseEntity.status(HttpStatus.OK).body(gson.toJson(response));
+    }
+
+    @PostMapping(path = "/convert-format", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Convert format")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Convert successful") })
+    public ResponseEntity<String> convertFormat(
+            @Validated @RequestBody(required = true) ConvertFormatRequestDto request,
+            @ApiIgnore Errors errors) {
+        ResponseDto response = new ResponseDto();
+        response.setVersion(version);
+        response.setResponsetime(serviceUtil.getCurrentResponseTime());
+        response.setResponse(
+            iBioApi.convertFormat(
+                    request.getSample(),
+                    request.getSourceFormat(),
+                    request.getTargetFormat(),
+                    request.getSourceParams(),
+                    request.getTargetParams(),
+                    request.getModalitiesToConvert()
+            )
+        );
+        return ResponseEntity.status(HttpStatus.OK).body(gson.toJson(response));
+    }
+
+    @PostMapping(path = "/segment", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Segment")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Segment successful") })
+    public ResponseEntity<String> segment(
+            @Validated @RequestBody(required = true) SegmentRequestDto request,
+            @ApiIgnore Errors errors) {
+        ResponseDto response = new ResponseDto();
+        response.setVersion(version);
+        response.setResponsetime(serviceUtil.getCurrentResponseTime());
+        response.setResponse(
+            iBioApi.segment(request.getSample(), request.getModalitiesToSegment(), request.getFlags())
+        );
+        return ResponseEntity.status(HttpStatus.OK).body(gson.toJson(response));
+    }
+}
