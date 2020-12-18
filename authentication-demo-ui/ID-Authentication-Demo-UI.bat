375bb8f5114d750604348f85b2ddcb299de13333
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2020-12-18 12:29:47 +0530

    using placeholder instead of actual values

--- authentication-demo-ui/ID-Authentication-Demo-UI.bat
+++ authentication-demo-ui/ID-Authentication-Demo-UI.bat
@@ -1,3 +1,3 @@
-# Note: -Dfinger.device.subid=1 added to support fingerprint slab device. For single fingerprint auth that argument can be removed
-java -Dida.request.captureFinger.deviceId=2 -Dida.request.captureIris.deviceId=1 -Dida.request.captureFace.deviceId=3 -Dmosip.base.url=https://qa2.mosip.net -DmispLicenseKey=BQN9arnVNTTOMnsP3SHWdS9pRiMrlc0jJKltNTegeCQqWwtfRU -DpartnerId=20001 -DpartnerApiKey=50993551 -Dfinger.device.subid=1 -jar ./target/authentication-demo-ui-1.2.0-SNAPSHOT.jar
+# Note: Use -Dfinger.device.subid=1 added to support fingerprint slab device. For single fingerprint auth that argument can be removed
+java -Dida.request.captureFinger.deviceId=<finger.device.id> -Dida.request.captureIris.deviceId=<iris.device.id> -Dida.request.captureFace.deviceId=<face.device.id> -Dmosip.base.url=<ida-base-url> -DmispLicenseKey=<misp.license.key> -DpartnerId=<partner.id> -DpartnerApiKey=<partner.api.key> -Dfinger.device.subid=<finger.device.subid> -jar ./target/authentication-demo-ui-1.2.0-SNAPSHOT.jar
 
