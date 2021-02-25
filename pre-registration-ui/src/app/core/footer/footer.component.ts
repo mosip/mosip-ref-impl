d9a8b11e7e9b4c0b3ee7ac4f52dd704b70a02b88
Ajay1596 <44763643+Ajay1596@users.noreply.github.com>
2021-02-25 17:00:46 +0530

    adding version from config

--- pre-registration-ui/src/app/core/footer/footer.component.ts
+++ pre-registration-ui/src/app/core/footer/footer.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { version } from 'package.json';
+import { Component, OnInit } from "@angular/core";
+import { DataStorageService } from "../services/data-storage.service";
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"],
 })
 export class FooterComponent implements OnInit {
-
-  appVersion = version;
-  constructor() { }
+  appVersion: string;
+  constructor(private dataService: DataStorageService) {}
 
   ngOnInit() {
+    this.dataService.getConfig().subscribe(res => {
+      this.appVersion = res['response']['preregistration.ui.version'];
+    });
   }
-
 }
