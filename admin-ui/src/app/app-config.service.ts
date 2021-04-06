aafce38e82d785165cbbf971a801bd8e2dabdcf2
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2021-04-06 16:17:34 +0530

    MOSIP-12726 Center New Multi Lang Design started

--- admin-ui/src/app/app-config.service.ts
+++ admin-ui/src/app/app-config.service.ts
@@ -1,39 +1,34 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HeaderService } from '../app/core/services/header.service';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { HeaderService } from "../app/core/services/header.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AppConfigService {
-
   appConfig: any;
 
-  constructor(
-    private http: HttpClient,
-    private headerService: HeaderService,
-  ) { }
+  constructor(private http: HttpClient, private headerService: HeaderService) {}
 
   async loadAppConfig() {
-    
-    this.appConfig = await this.http.get('./assets/config.json').toPromise();
-    if(this.appConfig.primaryLangCode){
-      this.appConfig["primaryLangCode"]= this.appConfig.primaryLangCode;
-      this.appConfig["secondaryLangCode"] = this.appConfig.secondaryLangCode;   
-      this.appConfig["supportedLanguages"] = this.appConfig.supportedLanguages;  
+    this.appConfig = await this.http.get("./assets/config.json").toPromise();
+    if (this.appConfig.primaryLangCode) {
+      this.appConfig["primaryLangCode"] = this.appConfig.primaryLangCode;
+      this.appConfig["secondaryLangCode"] = this.appConfig.secondaryLangCode;
 
-    	this.http.get(this.appConfig.baseUrl + 'masterdata/configs').subscribe(
-	      response => {
-	        this.appConfig["locationHierarchyLevel"]= response["response"]["locationHierarchyLevel"];
-          if (response["response"]["supportedLanguages"]) {
-            this.appConfig["supportedLanguages"]= response["response"]["supportedLanguages"];
-          }
-	      },
-	      error => {
-	        console.log(error);
-	      }
-	    );
-
+      this.http.get(this.appConfig.baseUrl + "masterdata/configs").subscribe(
+        (response) => {
+          let responseData = response["response"];
+          this.appConfig["locationHierarchyLevel"] = responseData["locationHierarchyLevel"];
+          this.appConfig["supportedLanguages"] = responseData["supportedLanguages"];
+          this.appConfig["rightToLeftOrientation"] = responseData["rightToLeftOrientation"];
+          this.appConfig["leftToRightOrientation"] = responseData["leftToRightOrientation"];
+          this.appConfig["countryCode"] = responseData["countryCode"];
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
   }
 
