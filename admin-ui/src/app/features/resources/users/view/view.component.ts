c30e535d462b1c52dd871082c3a16e87e0e7a9dc
Aravindhan Alagesan <aravindh2090@gmail.com>
2021-04-16 10:11:07 +0530

    User to zone mapping changes are inprogress

--- admin-ui/src/app/features/resources/users/view/view.component.ts
+++ admin-ui/src/app/features/resources/users/view/view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuditService } from 'src/app/core/services/audit.service';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfigService } from 'src/app/app-config.service';
+import * as userConfig from 'src/assets/entity-spec/user.json';
 
 @Component({
   selector: 'app-view',
@@ -9,6 +10,12 @@ import { AppConfigService } from 'src/app/app-config.service';
   styleUrls: ['./view.component.scss']
 })
 export class ViewComponent implements OnInit {
+
+  displayedColumns = [];
+  actionButtons = [];
+  actionEllipsis = [];
+  paginatorOptions: any;
+
   keycloakUrl =
     location.origin+'/keycloak/auth/admin/master/console/#/realms/mosip/users';
 
@@ -17,9 +24,22 @@ export class ViewComponent implements OnInit {
     private translate: TranslateService,
     private appService: AppConfigService
   ) {
+    this.getUserConfigs();
     translate.use(appService.getConfig()['primaryLangCode']);
   }
   ngOnInit() {
     this.auditService.audit(3, 'ADM-042', 'users');
+  }
+
+  getUserConfigs() {
+    this.displayedColumns = userConfig.columnsToDisplay;
+    console.log(this.displayedColumns);
+    this.actionButtons = userConfig.actionButtons.filter(
+      value => value.showIn.toLowerCase() === 'ellipsis'
+    );
+    this.actionEllipsis = userConfig.actionButtons.filter(
+      value => value.showIn.toLowerCase() === 'button'
+    );
+    this.paginatorOptions = userConfig.paginator;
   }
 }
