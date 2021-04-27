6260ff15e4a0346298bf8a005e5a8cb14b126761
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2021-04-27 19:27:55 +0530

    MOSIP-11079 changeAction handling in UI specs

--- pre-registration-ui/src/app/feature/file-upload/file-upload/file-upload.component.ts
+++ pre-registration-ui/src/app/feature/file-upload/file-upload/file-upload.component.ts
@@ -126,8 +126,6 @@ export class FileUploadComponent implements OnInit, OnDestroy {
 
   async ngOnInit() {
     await this.getIdentityJsonFormat();
-    console.log("mayura uiFields");
-    console.log(this.uiFields);
     this.getFileSize();
     this.getPrimaryLabels();
     this.allowedFiles = this.config
