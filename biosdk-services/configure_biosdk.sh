326ac9dc74354becd43feb681fc90b5a3b6b97b1
mandeepdhiman123 <mandeep.kumar@technoforte.co.in>
2021-05-26 20:17:35 +0530

    removed zipinfo step

--- biosdk-services/configure_biosdk.sh
+++ biosdk-services/configure_biosdk.sh
@@ -10,23 +10,27 @@ echo "Downloaded $biosdk_zip_file_path"
 
 FILE_NAME=${biosdk_zip_file_path##*/}
 
+echo $FILE_NAME
+
 DIR_NAME=$biosdk_local_dir_name
 
-has_parent=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | wc -l)
-if test "$has_parent" -eq 1; then
-  echo "Zip has a parent directory inside"
-  dirname=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | head -n 1)
-  echo "Unzip directory"
-  unzip $FILE_NAME
-  echo "Renaming directory"
-  mv -v $dirname $DIR_NAME
-else
-  echo "Zip has no parent directory inside"
-  echo "Creating destination directory"
-  mkdir "$DIR_NAME"
-  echo "Unzip to destination directory"
-  unzip -d "$DIR_NAME" $FILE_NAME
-fi
+#has_parent=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | wc -l)
+#if test "$has_parent" -eq 1; then
+#  echo "Zip has a parent directory inside"
+#  dirname=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"/");print a[1]}' | sort -u | head -n 1)
+#  echo "Unzip directory"
+#  unzip $FILE_NAME
+#  echo "Renaming directory"
+#  mv -v $dirname $DIR_NAME
+#else
+#  echo "Zip has no parent directory inside"
+#  echo "Creating destination directory"
+#  mkdir "$DIR_NAME"
+#  echo "Unzip to destination directory"
+#  unzip -d "$DIR_NAME" $FILE_NAME
+#fi
+
+unzip -d "$DIR_NAME" $FILE_NAME
 
 echo "Copying to loader path"
 cp -R ./$DIR_NAME/* $loader_path_env
