d0fb9e67a58644b2bb291333de47dab99a126051
Ajay1596 <ajay.j2@mindtree.com>
2021-06-10 12:09:47 +0530

    dashboard fixes

--- pre-registration-ui/src/app/feature/dashboard/dashboard/dashboard.component.ts
+++ pre-registration-ui/src/app/feature/dashboard/dashboard/dashboard.component.ts
@@ -825,6 +825,7 @@ export class DashBoardComponent implements OnInit, OnDestroy {
     if (value === appConstants.APPLICATION_STATUS_CODES.booked) return "green";
     if (value === appConstants.APPLICATION_STATUS_CODES.expired) return "red";
     if (value === appConstants.APPLICATION_STATUS_CODES.cancelled) return "purple";
+    if (value === "Application_Incomplete") return "maroon";
   }
 
   getMargin(name: string) {
