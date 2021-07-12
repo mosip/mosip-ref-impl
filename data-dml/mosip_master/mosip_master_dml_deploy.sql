0198761688d9c232865c7547fed2747154bc396b
rambhatt1591 <77657115+rambhatt1591@users.noreply.github.com>
2021-07-12 17:20:18 +0530

    MOSIP-14914 rcid dml creation and registration center changes,MOSIP-15058 mid-seq table changes,MOSIP-15167,MOSIP-14729 (#578)
    * MOSIP-14914 rcid dml creation and registration center changes
    
    * MOSIP-15058 mid-seq table changes

--- data-dml/mosip_master/mosip_master_dml_deploy.sql
+++ data-dml/mosip_master/mosip_master_dml_deploy.sql
@@ -59,6 +59,12 @@ TRUNCATE TABLE master.machine_type cascade ;
 
 \COPY master.machine_type (code,name,descr,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/master-machine_type.csv' delimiter ',' HEADER  csv;
 
+----- TRUNCATE master.mid_seq TABLE Data and It's reference Data and COPY Data from CSV file -----
+TRUNCATE TABLE master.mid_seq cascade;
+
+\COPY master.mid_seq (curr_seq_no,cr_by ,cr_dtimes ) FROM './dml/master-mid_seq.csv' delimiter ',' HEADER  csv;
+
+
 ----- TRUNCATE master.module_detail TABLE Data and It's reference Data and COPY Data from CSV file -----
 TRUNCATE TABLE master.module_detail cascade ;
 
@@ -68,6 +74,11 @@ TRUNCATE TABLE master.module_detail cascade ;
 TRUNCATE TABLE master.process_list cascade ;
 
 \COPY master.process_list (id,name,descr,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/master-process_list.csv' delimiter ',' HEADER  csv;
+
+----- TRUNCATE master.rcid_seq TABLE Data and It's reference Data and COPY Data from CSV file -----
+TRUNCATE TABLE master.rcid_seq cascade;
+
+\COPY master.rcid_seq (curr_seq_no,cr_by ,cr_dtimes ) FROM './dml/master-rcid_seq.csv' delimiter ',' HEADER  csv;
 
 ----- TRUNCATE master.reason_category TABLE Data and It's reference Data and COPY Data from CSV file -----
 TRUNCATE TABLE master.reason_category cascade ;
