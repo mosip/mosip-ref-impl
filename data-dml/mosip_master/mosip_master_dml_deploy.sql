8aeb6606b6aabd4f08ef38b22a8848a5fec01bff
rambhatt1591 <77657115+rambhatt1591@users.noreply.github.com>
2021-07-12 19:11:02 +0530

    MOSIP-15588 blacklisted words dml upload (#597)

--- data-dml/mosip_master/mosip_master_dml_deploy.sql
+++ data-dml/mosip_master/mosip_master_dml_deploy.sql
@@ -24,6 +24,11 @@ TRUNCATE TABLE master.blacklisted_words cascade ;
 
 \COPY master.blacklisted_words (word,descr,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/master-blacklisted_words.csv' delimiter ',' HEADER  csv;
 
+----- TRUNCATE master.blocklisted_words TABLE Data and It's reference Data and COPY Data from CSV file -----
+TRUNCATE TABLE master.blocklisted_words cascade ;
+
+\COPY master.blocklisted_words (word,descr,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/master-blocklisted_words.csv' delimiter ',' HEADER  csv;
+
 ----- TRUNCATE master.device_type TABLE Data and It's reference Data and COPY Data from CSV file -----
 TRUNCATE TABLE master.device_type cascade ;
 
