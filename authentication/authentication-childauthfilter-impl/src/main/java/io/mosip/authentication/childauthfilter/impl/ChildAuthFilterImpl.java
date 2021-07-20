2c3ba2ae9f5491f97b91cd4b8c59a6f1db0b9911
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-07-20 17:35:48 +0530

    Added unit tests

--- authentication/authentication-childauthfilter-impl/src/main/java/io/mosip/authentication/childauthfilter/impl/ChildAuthFilterImpl.java
+++ authentication/authentication-childauthfilter-impl/src/main/java/io/mosip/authentication/childauthfilter/impl/ChildAuthFilterImpl.java
@@ -12,7 +12,6 @@ import io.mosip.authentication.authfilter.exception.IdAuthenticationFilterExcept
 import io.mosip.authentication.authfilter.spi.IMosipAuthFilter;
 import io.mosip.authentication.core.constant.IdAuthCommonConstants;
 import io.mosip.authentication.core.constant.IdAuthenticationErrorConstants;
-import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
 import io.mosip.authentication.core.indauth.dto.AuthRequestDTO;
 import io.mosip.authentication.core.indauth.dto.IdentityInfoDTO;
 import io.mosip.kernel.core.util.DateUtils;
@@ -27,39 +26,49 @@ import io.mosip.kernel.core.util.DateUtils;
 public class ChildAuthFilterImpl implements IMosipAuthFilter {
 	
 	
+	/** The Constant OTP. */
 	private static final String OTP = "otp";
 
+	/** The Constant DEMO. */
 	private static final String DEMO = "demo";
 
+	/** The Constant BIO. */
 	private static final String BIO = "bio";
 
+	/** The Constant DEFAULT_CHILD_MAX_AGE. */
 	private static final int DEFAULT_CHILD_MAX_AGE = 6;
 
+	/** The date of birth attribute name. */
 	@Value("${ida.child-auth-filter.date-of-birth.attribute.name:dateOfBirth}")
 	private String dateOfBirthAttributeName;
 	
+	/** The date of birth pattern. */
 	@Value("${ida.child-auth-filter.date-of-birth.pattern:" + IdAuthCommonConstants.DOB_PATTERN + "}")
 	private String dateOfBirthPattern;
 	
+	/** The child max age. */
 	@Value("${ida.child-auth-filter.child.max.age:" + DEFAULT_CHILD_MAX_AGE + "}")
 	private int childMaxAge;
 	
+	/** The factors denied for child. */
 	@Value("${ida.child-auth-filter.factors.denied:otp,bio}")
 	private String[] factorsDeniedForChild;
 
 	/**
-	 * Inits the.
+	 * Initializes the filter.
+	 *
+	 * @throws IdAuthenticationFilterException the id authentication filter exception
 	 */
 	public void init() throws IdAuthenticationFilterException {
 	}
 
 	/**
-	 * Test method that executes predicate test condition on the given arguments
+	 * Test method that executes predicate test condition on the given arguments.
 	 *
 	 * @param authRequest  the auth request
 	 * @param identityData the identity data
 	 * @param properties   the properties
-	 * @throws IdAuthenticationBusinessException 
+	 * @throws IdAuthenticationFilterException the id authentication filter exception
 	 */
 	public void validate(AuthRequestDTO authRequest, Map<String, List<IdentityInfoDTO>> identityData,
 			Map<String, Object> properties) throws IdAuthenticationFilterException {
@@ -69,6 +78,12 @@ public class ChildAuthFilterImpl implements IMosipAuthFilter {
 		}
 	}
 
+	/**
+	 * Check denied factors for child.
+	 *
+	 * @param authRequest the auth request
+	 * @throws IdAuthenticationFilterException the id authentication filter exception
+	 */
 	private void checkDeniedFactorsForChild(AuthRequestDTO authRequest) throws IdAuthenticationFilterException{
 		List<Object> deniedFactors = Stream.of(factorsDeniedForChild)
 											.map(String::toLowerCase)
@@ -93,6 +108,13 @@ public class ChildAuthFilterImpl implements IMosipAuthFilter {
 		
 	}
 
+	/**
+	 * Gets the date of birth.
+	 *
+	 * @param dobData the dob data
+	 * @return the date of birth
+	 * @throws IdAuthenticationFilterException the id authentication filter exception
+	 */
 	private LocalDate getDateOfBirth(List<IdentityInfoDTO> dobData) throws IdAuthenticationFilterException {
 		if(dobData == null || dobData.isEmpty()) {
 			throw new IdAuthenticationFilterException(IdAuthenticationErrorConstants.ID_NOT_AVAILABLE.getErrorCode(), String.format(IdAuthenticationErrorConstants.ID_NOT_AVAILABLE.getErrorMessage(), dateOfBirthAttributeName));
