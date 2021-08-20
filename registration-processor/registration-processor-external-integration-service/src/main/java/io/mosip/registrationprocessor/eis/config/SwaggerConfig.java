2f195faf41fcc892d9ae26c452f8fce0315ddd1a
kameshsr <kameshsr1338@gmail.com>
2021-08-20 17:58:25 +0530

    MOSIP-15857: Update swagger2 to openapi3.0 for registration external integration service.

--- registration-processor/registration-processor-external-integration-service/src/main/java/io/mosip/registrationprocessor/eis/config/SwaggerConfig.java
+++ registration-processor/registration-processor-external-integration-service/src/main/java/io/mosip/registrationprocessor/eis/config/SwaggerConfig.java
@@ -3,28 +3,24 @@ package io.mosip.registrationprocessor.eis.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 /**
  * external Configuration
  *
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 	/**
 	 * DummyBean method for swagger configuration
 	 * @return
 	 */
-	@Bean
-	public Docket DummyBean() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("external-integration-service").select()
-				.apis(RequestHandlerSelectors.basePackage("io.mosip.registrationprocessor.eis.controller"))
-				.paths(PathSelectors.ant("/registration-processor/*/*")).build();
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("External Integration Service API documentation").description(
+						"External Integration service ").version("3.0.1"));
 	}
-
 }
