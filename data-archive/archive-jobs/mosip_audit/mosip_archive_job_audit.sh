2f47dcf54460036e829e769acad5488187a0635c
ckm007 <chandrakeshavmishra@gmail.com>
2021-09-02 13:17:55 +0000

    [DSD-308] added archieval job for audit

---
+++ data-archive/archive-jobs/mosip_audit/mosip_archive_job_audit.sh
@@ -0,0 +1,13 @@
+### -- ---------------------------------------------------------------------------------------------------------
+### -- Script Name		: Audit Archive Job
+### -- Deploy Module 		: Audit
+### -- Purpose    		: To Archive Audit DB tables which are marked for archive.       
+### -- Create By   		: Chandra Keshav Mishra
+### -- Created Date		: Sept-2021
+### -- 
+### -- Modified Date        Modified By         Comments / Remarks
+### -- ----------------------------------------------------------------------------------------
+
+python mosip_archive_audit_table1.py &
+
+#===============================================================================================
