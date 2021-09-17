e3ea5b92f5cc42b19c573a5384fed2c246459e6d
root <root@console.sb>
2021-09-17 09:50:37 +0000

    DSD[308] added changes for credential  DB archival

---
+++ data-archive/archive-jobs/mosip_credential/mosip_archive_job_credential.sh
@@ -0,0 +1,8 @@
+### -- ---------------------------------------------------------------------------------------------------------
+### -- Script Name		: Credential Archive Job
+### -- Deploy Module 		: Credentail DB (IDREPO)
+### -- Purpose    		: To Archive credential DB tables which are marked for archive.       
+### -- Create By   		: Chandra Keshav Mishra
+### -- Created Date		: Sept-2021
+
+python3 mosip_archive_credential_table1.py
