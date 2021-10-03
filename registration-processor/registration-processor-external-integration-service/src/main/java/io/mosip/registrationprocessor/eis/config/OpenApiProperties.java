74dc3463eda28f89946b773e8f71aef74413ed5e
kameshsr <kameshsr1338@gmail.com>
2021-10-03 11:25:58 +0530

    MOSIP-15857: Fixed https to https problem for external integration service

---
+++ registration-processor/registration-processor-external-integration-service/src/main/java/io/mosip/registrationprocessor/eis/config/OpenApiProperties.java
@@ -0,0 +1,41 @@
+package io.mosip.registrationprocessor.eis.config;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix = "openapi")
+@Data
+public class OpenApiProperties {
+    private InfoProperty info;
+    private RegistrationProcessorExternalIntegrationService registrationProcessorExternalIntegrationService;
+}
+
+@Data
+class InfoProperty {
+    private String title;
+    private String description;
+    private String version;
+    private LicenseProperty license;
+}
+
+@Data
+class LicenseProperty {
+    private String name;
+    private String url;
+}
+
+@Data
+class RegistrationProcessorExternalIntegrationService {
+    private List<Server> servers;
+}
+
+@Data
+class Server {
+    private String description;
+    private String url;
+}
