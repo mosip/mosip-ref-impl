dc548dd13519006b17f824be4a468c6a8b36f00b
kameshsr <47484458+kameshsr@users.noreply.github.com>
2021-10-21 15:52:35 +0530

    MOSIP-15857: Fixed http to https problem for external integration service. (#745)
    * MOSIP-15857: Fixed https to https problem for external integration service
    
    * Updated openapi properties
    
    * Added openapi properties

--- registration-processor/registration-processor-external-integration-service/src/main/java/io/mosip/registrationprocessor/eis/config/SwaggerConfig.java
+++ registration-processor/registration-processor-external-integration-service/src/main/java/io/mosip/registrationprocessor/eis/config/SwaggerConfig.java
@@ -1,26 +1,61 @@
 package io.mosip.registrationprocessor.eis.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
 /**
- * external Configuration
+ * Configuration class for swagger config
+ * @implSpec upgrade the Swagger2.0 to OpenAPI (Swagger3.0)
  *
  */
 @Configuration
 public class SwaggerConfig {
-	/**
-	 * DummyBean method for swagger configuration
-	 * @return
-	 */
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI()
-				.components(new Components())
-				.info(new Info().title("External Integration Service API documentation").description(
-						"External Integration service ").version("3.0.1"));
+
+	private static final Logger logger = LoggerFactory.getLogger(SwaggerConfig.class);
+
+	@Autowired
+	private OpenApiProperties openApiProperties;
+
+	@Bean
+	public OpenAPI openApi() {
+		String msg = "Swagger open api, ";
+		OpenAPI api = new OpenAPI()
+				.components(new Components());
+		if (null != openApiProperties.getInfo()) {
+			api.info(new Info()
+					.title(openApiProperties.getInfo().getTitle())
+					.version(openApiProperties.getInfo().getVersion())
+					.description(openApiProperties.getInfo().getDescription()));
+			if (null != openApiProperties.getInfo().getLicense()) {
+				api.getInfo().license(new License()
+						.name(openApiProperties.getInfo().getLicense().getName())
+						.url(openApiProperties.getInfo().getLicense().getUrl()));
+				logger.info(msg + "info license property is added");
+			} else {
+				logger.error(msg + "info license property is empty");
+			}
+			logger.info(msg + "info property is added");
+		} else {
+			logger.error(msg + "info property is empty");
+		}
+		if (null != openApiProperties.getRegistrationProcessorExternalIntegrationService().getServers()) {
+			openApiProperties.getRegistrationProcessorExternalIntegrationService().getServers().forEach(server -> {
+				api.addServersItem(new Server().description(server.getDescription()).url(server.getUrl()));
+			});
+			logger.info(msg + "server property is added");
+		} else {
+			logger.error(msg + "server property is empty");
+		}
+		return api;
 	}
+
 }
