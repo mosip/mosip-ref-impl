1d7212c0c01af1fdb894be754710eb61186afd7b
kameshsr <kameshsr1338@gmail.com>
2022-08-12 11:19:54 +0530

    MOSIP-23441 Archival script for crdential_transaction

--- data-archive/archive-jobs/mosip_credential/mosip_archive_job_credential.sh
+++ data-archive/archive-jobs/mosip_credential/mosip_archive_job_credential.sh
@@ -2,7 +2,7 @@
 ### -- Script Name		: Credential Archive Job
 ### -- Deploy Module 		: Credentail DB (IDREPO)
 ### -- Purpose    		: To Archive credential DB tables which are marked for archive.       
-### -- Create By   		: Chandra Keshav Mishra
-### -- Created Date		: Sept-2021
+### -- Create By   		: Kamesh Shekhar Prasad
+### -- Created Date		: Aug-2022
 
 python3 mosip_archive_credential_table1.py
