0679e2e40a32eb0f98e2943b5b268dd7b41e1948
Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
2024-06-10 15:35:52 +0530

    merged from java 21 and updated some depedencies
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>

--- registration-processor/registration-processor-external-stage/src/main/java/io/mosip/registrationprocessor/externalstage/ExternalStageApplication.java
+++ registration-processor/registration-processor-external-stage/src/main/java/io/mosip/registrationprocessor/externalstage/ExternalStageApplication.java
@@ -2,7 +2,6 @@ package io.mosip.registrationprocessor.externalstage;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import io.mosip.registration.processor.core.config.reader.ConfigPropertyReader;
 import io.mosip.registrationprocessor.externalstage.stage.ExternalStage;
 
 /**
@@ -19,7 +18,6 @@ public class ExternalStageApplication {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
 		configApplicationContext.scan("io.mosip.registration.processor.core.config",
-				ConfigPropertyReader.getConfig("mosip.auth.adapter.impl.basepackage"),
 				"io.mosip.registrationprocessor.externalstage.config", "io.mosip.registration.processor.core.config",
 				"io.mosip.registration.processor.status.config", "io.mosip.registration.processor.core.kernel.beans",
 				"io.mosip.registration.processor.rest.client.config");
