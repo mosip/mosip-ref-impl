e3207aacf4c399ef315edbb239354eca388bee39
Aiham <aiham.hasan@mindtree.com>
2024-06-25 14:31:51 +0530

    Updated code
    Signed-off-by: Aiham <aiham.hasan@mindtree.com>

--- pre-registration-booking-service/src/main/java/io/mosip/preregistration/booking/service/util/BookingServiceUtil.java
+++ pre-registration-booking-service/src/main/java/io/mosip/preregistration/booking/service/util/BookingServiceUtil.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
