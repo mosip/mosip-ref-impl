3b9f6dbb24796acbf52fcb847f19dece440e95d6
kameshsr <kameshsr1338@gmail.com>
2024-11-06 10:30:10 +0530

    Fixed test case
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- kernel/kernel-smsserviceprovider-msg91/src/test/java/io/mosip/kernel/smsserviceprovider/msg91/test/SmsServiceProviderTest.java
+++ kernel/kernel-smsserviceprovider-msg91/src/test/java/io/mosip/kernel/smsserviceprovider/msg91/test/SmsServiceProviderTest.java
@@ -66,6 +66,9 @@ public class SmsServiceProviderTest {
 	@Value("${mosip.id.validation.identity.phone}")
 	private String phoneRegex;
 
+	@Value("${phone}")
+	private String phone;
+
 	@Test
 	public void sendSmsTest() {
 
@@ -111,7 +114,7 @@ public class SmsServiceProviderTest {
 
 	@Test
 	public void validGateWayTest() {
-		service.sendSms("+2987654321", "hello your otp is 45373");
+		service.sendSms(phone, "hello your otp is 45373");
 	}
 
 }
