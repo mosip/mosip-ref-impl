da0941547da873381566d7f705b5a5ec46900f77
ase-101 <sunkadaeanusha@gmail.com>
2025-07-25 14:12:32 +0530

    MOSIP-42452
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- kernel/kernel-smsserviceprovider-msg91/src/test/java/io/mosip/kernel/smsserviceprovider/msg91/test/SmsServiceProviderTest.java
+++ kernel/kernel-smsserviceprovider-msg91/src/test/java/io/mosip/kernel/smsserviceprovider/msg91/test/SmsServiceProviderTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +26,7 @@ import io.mosip.kernel.smsserviceprovider.msg91.dto.SmsServerResponseDto;
 import io.mosip.kernel.smsserviceprovider.msg91.impl.SMSServiceProviderImpl;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = { ConfigDataApplicationContextInitializer.class, SmsServiceProviderTest.config.class,
+@ContextConfiguration(classes = { SmsServiceProviderTest.config.class,
 		SMSServiceProviderImpl.class })
 public class SmsServiceProviderTest {
 
